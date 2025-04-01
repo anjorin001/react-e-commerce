cb73bec5dcfb73b6eb1f058b187bc2d6f071572f
anjorin001 <anjorinfavour123@gmail.com>
2025-04-01 18:39:30 +0100

    removed basename from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/e-commerce">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
